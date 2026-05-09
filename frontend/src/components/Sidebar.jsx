f929711986dd15d302a4f7d8af0d4490142c377f
yoshirodev <r.yoshiki0000@gmail.com>
2026-05-09 20:03:39 +0800

    fixed the  Font Awesom Loads and the Sidebar Logo

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 export default function Sidebar({ today }) {
     return (
         <div className="sidebar">
+
+            <div className="logo-box">
+                <img src="../../public/logo.png" alt="Logo" />
+            </div>
+
             <div className="date-box">
                 <p><strong>Today</strong></p>
                 <p>{today}</p>
